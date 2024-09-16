4cbbf02f62a0537b30754d8cdf6a925778a2c544
Sebastian Skontos <ssko7098@uni.sydney.edu.au>
2024-09-16 19:39:09 +1000

    renamed user and profile PKs to 'id' for simplicity

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=20, unique=True)
     full_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=30)
@@ -13,7 +13,7 @@ class User(models.Model):
         return self.username
 
 class Profile(models.Model):
-    profile_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(null=True, blank=True)
     avatar_url = models.CharField(max_length=50, null=True, blank=True)
