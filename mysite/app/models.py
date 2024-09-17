e0f6fa1edffa861081d75a1deb66ea1c779608ba
Sebastian Skontos <ssko7098@uni.sydney.edu.au>
2024-09-17 16:10:45 +1000

    created models for all other classes (incl. album, song, playlist, comment)

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -20,3 +20,51 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Album(models.Model):
+    album_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+class Song(models.Model):
+    song_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    duration = models.IntegerField()  # duration in seconds
+
+    def __str__(self):
+        return self.title
+
+class Playlist(models.Model):
+    playlist_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class PlaylistSong(models.Model):
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('playlist', 'song')
+
+    def __str__(self):
+        return f'{self.playlist.name} - {self.song.title}'
+
+class Comment(models.Model):
+    comment_id = models.AutoField(primary_key=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.user.username}'
+
