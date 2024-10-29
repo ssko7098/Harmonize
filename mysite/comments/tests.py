b2d1b3a772595fc6f27c34e2d342c3d9f9b712c1
ryantanevski <12ryantan12@gmail.com>
2024-10-29 20:12:56 +1100

    added first tests for comments

--- mysite/comments/tests.py
+++ mysite/comments/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from .models import Comment
+from users.models import User, Profile
 
-# Create your tests here.
+User = get_user_model()
+
+class CommentViewsTestCase(TestCase):
+    def setUp(self):
+        # Create users
+        self.user1 = User.objects.create_user(username='user1', password='password', email='user1@example.com')
+        self.user2 = User.objects.create_user(username='user2', password='password', email='user2@example.com')
+        
+        # Create profiles
+        self.profile = Profile.objects.create(user=self.user2, bio='Bio of User Two')
+
+        # Log in user1
+        self.client.login(username='user1', password='password')
+
+    def test_add_comment(self):
+        response = self.client.post(reverse('add_comment', args=[self.user2.username]), {
+            'message': 'This is a comment!'
+        })
+        self.assertEqual(response.status_code, 302)  # Check redirect
+        self.assertEqual(Comment.objects.count(), 1)  # One comment should exist
+        comment = Comment.objects.first()
+        self.assertEqual(comment.message, 'This is a comment!')
+        self.assertEqual(comment.user, self.user1)
+        self.assertEqual(comment.profile, self.profile)
