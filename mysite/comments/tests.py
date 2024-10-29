f7c4c3336e0eb64441fb32500a6a4bba291ffcce
ryantanevski <12ryantan12@gmail.com>
2024-10-29 20:54:58 +1100

    added tests for adding comments

--- mysite/comments/tests.py
+++ mysite/comments/tests.py
@@ -28,3 +28,13 @@ class CommentViewsTestCase(TestCase):
         self.assertEqual(comment.message, 'This is a comment!')
         self.assertEqual(comment.user, self.user1)
         self.assertEqual(comment.profile, self.profile)
+
+    def test_add_comment_to_own_profile(self):
+        self.client.logout()
+        self.client.login(username='user2', password='password')  # User2 logs in
+        response = self.client.post(reverse('add_comment', args=[self.user2.username]), {
+            'message': 'This should not be added.'
+        })
+        self.assertEqual(response.status_code, 302)  # Check redirect
+        self.assertEqual(Comment.objects.count(), 0)  # No new comments should be added
+
