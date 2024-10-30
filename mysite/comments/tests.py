df5b15a80c7d24f4bd55e7e5e4769c8c82a33ed5
ryantanevski <12ryantan12@gmail.com>
2024-10-30 21:50:54 +1100

    added tests for Comment form

--- mysite/comments/tests.py
+++ mysite/comments/tests.py
@@ -2,7 +2,9 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from .models import Comment
+from .forms import CommentForm
 from users.models import User, Profile
+from django import forms
 
 User = get_user_model()
 
@@ -18,8 +20,8 @@ class CommentViewsTestCase(TestCase):
         # Log in user1
         self.client.login(username='user1', password='password')
 
-    # tests if a comment can be added by a user to anothers profile
     def test_add_comment(self):
+        """tests if a comment can be added by a user to anothers profile"""
         response = self.client.post(reverse('add_comment', args=[self.user2.username]), {
             'message': 'This is a comment!'
         })
@@ -30,8 +32,8 @@ class CommentViewsTestCase(TestCase):
         self.assertEqual(comment.user, self.user1)
         self.assertEqual(comment.profile, self.profile)
 
-    #tests if a user is prevented from commenting on their own profile
     def test_add_comment_to_own_profile(self):
+        """tests if a user is prevented from commenting on their own profile"""
         self.client.logout()
         self.client.login(username='user2', password='password')  # User2 logs in
         response = self.client.post(reverse('add_comment', args=[self.user2.username]), {
@@ -40,22 +42,22 @@ class CommentViewsTestCase(TestCase):
         self.assertEqual(response.status_code, 302)  # Check redirect
         self.assertEqual(Comment.objects.count(), 0)  # No new comments should be added
     
-    #tests if a user can delete their own comment
     def test_delete_comment(self):
+        """tests if a user can delete their own comment"""
         comment = Comment.objects.create(message='A comment', user=self.user1, profile=self.user2.profile)
         response = self.client.post(reverse('delete_comment', args=[comment.comment_id]))
         self.assertEqual(response.status_code, 302)  # Check redirect
         self.assertEqual(Comment.objects.count(), 0)  # Comment should be deleted
 
-    #tests if a user can delete another persons comment
     def test_delete_comment_not_owner(self):
+        """tests if a user can delete another persons comment"""
         comment = Comment.objects.create(message='A comment', user=self.user2, profile=self.profile)
         response = self.client.post(reverse('delete_comment', args=[comment.comment_id]))
         self.assertEqual(response.status_code, 302)  # Check redirect
         self.assertEqual(Comment.objects.count(), 1)  # Comment should still exist
 
-    #tests if a user can like a comment
     def test_like_comment(self):
+        """tests if a user can like a comment"""
         comment = Comment.objects.create(message='A comment', user=self.user2, profile=self.profile)
         response = self.client.post(reverse('like_comment', args=[comment.comment_id]))
         self.assertEqual(response.status_code, 302)  # Check redirect
@@ -63,8 +65,8 @@ class CommentViewsTestCase(TestCase):
         self.assertIn(self.user1, comment.liked_by.all())  # User1 should like the comment
         self.assertEqual(comment.likes, 1)  # Likes should be 1
 
-    #tests if a user likes a comment twice, the comment is unliked
     def test_double_like_comment(self):
+        """tests if a user likes a comment twice, the comment is unliked"""
         comment = Comment.objects.create(message='A comment', user=self.user2, profile=self.profile)
         self.client.post(reverse('like_comment', args=[comment.comment_id]))
         response = self.client.post(reverse('like_comment', args=[comment.comment_id]))
@@ -73,8 +75,8 @@ class CommentViewsTestCase(TestCase):
         self.assertNotIn(self.user1, comment.liked_by.all())  # User1 shouldnt like the comment
         self.assertEqual(comment.likes, 0)  # Likes should be 0, since like was pressed again
 
-    #tests if users can dislike a comment
     def test_dislike_comment(self):
+        """tests if users can dislike a comment"""
         comment = Comment.objects.create(message='A comment', user=self.user2, profile=self.profile)
         response = self.client.post(reverse('dislike_comment', args=[comment.comment_id]))
         self.assertEqual(response.status_code, 302)  # Check redirect
@@ -82,8 +84,8 @@ class CommentViewsTestCase(TestCase):
         self.assertIn(self.user1, comment.disliked_by.all())  # User1 should dislike the comment
         self.assertEqual(comment.dislikes, 1)  # Dislikes should be 1
 
-    #tests if a user dislikes a comment twice,the comment is un-disliked
     def test_double_dislike_comment(self):
+        """tests if a user dislikes a comment twice,the comment is un-disliked"""
         comment = Comment.objects.create(message='A comment', user=self.user2, profile=self.profile)
         self.client.post(reverse('dislike_comment', args=[comment.comment_id]))
         response = self.client.post(reverse('dislike_comment', args=[comment.comment_id]))
@@ -92,8 +94,8 @@ class CommentViewsTestCase(TestCase):
         self.assertNotIn(self.user1, comment.disliked_by.all())  # User1 shouldnt dislike the comment
         self.assertEqual(comment.dislikes, 0)  # Likes should be 1
 
-    #tests if a user switches from like to dislike, the other actions is reversed
     def test_switch_like_comment(self):
+        """tests if a user switches from like to dislike, the other actions is reversed"""
         comment = Comment.objects.create(message='A comment', user=self.user2, profile=self.profile)
         self.client.post(reverse('like_comment', args=[comment.comment_id]))  # Like first
         response = self.client.post(reverse('dislike_comment', args=[comment.comment_id]))
@@ -104,16 +106,16 @@ class CommentViewsTestCase(TestCase):
         self.assertIn(self.user1, comment.disliked_by.all())  # User1 should dislike the comment
         self.assertEqual(comment.dislikes, 1)  # Dislikes should be 1
 
-    #tests that users can report another users comment
     def test_report_comment(self):
+        """tests that users can report another users comment"""
         comment = Comment.objects.create(message='A comment', user=self.user2, profile=self.profile)
         response = self.client.post(reverse('report_comment', args=[comment.comment_id]))
         self.assertEqual(response.status_code, 302)  # Check redirect
         comment.refresh_from_db()
         self.assertEqual(comment.report_count, 1)  # Report count should be 1
 
-    #tests that users can not report a comment more than once
     def test_report_comment_already_reported(self):
+        """tests that users can not report a comment more than once"""
         comment = Comment.objects.create(message='A comment', user=self.user2, profile=self.profile)
         self.client.post(reverse('report_comment', args=[comment.comment_id]))  # First report
         response = self.client.post(reverse('report_comment', args=[comment.comment_id]))  # Second report
@@ -132,8 +134,8 @@ class ReplyCommentTest(TestCase):
         self.comment = Comment.objects.create(user=self.user, profile=self.profile, message='Original Comment')
         self.client.login(username='testuser', password='password')
     
-    #tests a user can reply to a comment
     def test_add_reply_to_comment(self):
+        """tests a user can reply to a comment"""
         # Add a reply to the original comment
         response = self.client.post(reverse('add_comment', args=[self.user.username]), {
             'message': 'Reply to Comment',
@@ -149,8 +151,8 @@ class ReplyCommentTest(TestCase):
         reply = Comment.objects.get(message='Reply to Comment')
         self.assertEqual(reply.parent_comment, self.comment)  # Ensure reply is linked to the parent
 
-    #tests that a reply appears correctly on profile page
     def test_view_comment_with_replies(self):
+        """tests that a reply appears correctly on profile page"""
         # Add a reply to the original comment
         reply = Comment.objects.create(user=self.user, profile=self.profile, message='Reply to Comment', parent_comment=self.comment)
         
@@ -166,8 +168,8 @@ class ReplyCommentTest(TestCase):
         comment_with_reply = Comment.objects.get(comment_id=self.comment.comment_id)
         self.assertIn(reply, comment_with_reply.replies.all())  # Check if the reply exists in the original comment's replies
 
-    #tests that a reply can be deleted
     def test_delete_reply_to_comment(self):
+        """tests that a reply can be deleted"""
         # Create a reply to the original comment
         reply = Comment.objects.create(user=self.user, profile=self.profile, message='Reply to Comment', parent_comment=self.comment)
         
@@ -178,8 +180,8 @@ class ReplyCommentTest(TestCase):
         # Ensure the reply no longer exists
         self.assertFalse(Comment.objects.filter(comment_id=reply.comment_id).exists())
 
-    #tests that a if a comment is deleted, the replies to the comment are also deleted
     def test_delete_comment_with_replies(self):
+        """tests that a if a comment is deleted, the replies to the comment are also deleted"""
         # Add a reply to the original comment
         reply = Comment.objects.create(user=self.user, profile=self.profile, message='Reply to Comment', parent_comment=self.comment)
         
@@ -269,3 +271,51 @@ class CommentModelTest(TestCase):
         """Test the string representation of the comment."""
         comment = Comment.objects.create(profile=self.profile1, user=self.user1, message='String rep test')
         self.assertEqual(str(comment), f'Comment by {comment.user.username}')
+
+class CommentFormTest(TestCase):
+    def setUp(self):
+        # Set up a user and profile for testing purposes
+        self.user = User.objects.create_user(username='testuser', password='password', email='test@example.com')
+        self.profile = Profile.objects.create(user=self.user, bio='Test bio')
+
+    def test_valid_form(self):
+        """Test that a form with valid data is considered valid."""
+        form_data = {'message': 'This is a valid comment.'}
+        form = CommentForm(data=form_data)
+        self.assertTrue(form.is_valid())
+    
+    def test_missing_message_field(self):
+        """Test that the form is invalid if the message field is missing."""
+        form_data = {}  # Missing 'message' field
+        form = CommentForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('message', form.errors)  # Check if 'message' field has errors
+        self.assertEqual(form.errors['message'], ['This field is required.'])
+    
+    def test_blank_message(self):
+        """Test that the form rejects a blank message."""
+        form_data = {'message': ''}
+        form = CommentForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('message', form.errors)  # Ensure 'message' has an error
+        self.assertEqual(form.errors['message'], ['This field is required.'])  # Check for required field error
+    
+    def test_message_length(self):
+        """Test that a message of valid length passes and very long message fails."""
+        # Valid message
+        valid_message = 'a' * 255 
+        form_data = {'message': valid_message}
+        form = CommentForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        
+        # Extremely long message
+        long_message = 'a' * 5000 
+        form_data = {'message': long_message}
+        form = CommentForm(data=form_data)
+        self.assertTrue(form.is_valid()) 
+    
+    def test_custom_widget_attrs(self):
+        """Test that the message field uses the correct widget attributes."""
+        form = CommentForm()
+        self.assertIsInstance(form.fields['message'].widget, forms.Textarea)
+        self.assertEqual(form.fields['message'].widget.attrs['rows'], 3)  # Check if the custom widget has 3 rows
